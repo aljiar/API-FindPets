06084b2ded8deaa3953d6663d59c80207ecdc071
Alan Jimenez <aljiar23@gmail.com>
2020-11-21 17:11:49 -0400

    Get method

--- src/main/java/com/internalPractice/APIFindPets/ApiFindPetsApplication.java
+++ src/main/java/com/internalPractice/APIFindPets/ApiFindPetsApplication.java
@@ -9,5 +9,9 @@ public class ApiFindPetsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFindPetsApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 
 }
