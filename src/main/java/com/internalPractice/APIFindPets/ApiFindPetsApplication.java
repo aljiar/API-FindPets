901f1100fc11421b8a10f5606ba6fda38a9cff30
Alan Jimenez <aljiar23@gmail.com>
2020-11-21 15:45:37 -0400

    Start project

---
+++ src/main/java/com/internalPractice/APIFindPets/ApiFindPetsApplication.java
@@ -0,0 +1,13 @@
+package com.internalPractice.APIFindPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFindPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFindPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internalPractice/APIFindPets/ApiFindPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internalPractice.APIFindPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFindPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
