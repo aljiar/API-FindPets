eeb1689738b8144af5dfb314525ae0699128fb2f
Alan Jimenez <aljiar23@gmail.com>
2020-11-23 19:20:36 -0400

    Setup databse connection

--- src/main/java/com/internalPractice/APIFindPets/ApiFindPetsApplication.java
+++ src/main/java/com/internalPractice/APIFindPets/ApiFindPetsApplication.java
@@ -3,15 +3,12 @@ package com.internalPractice.APIFindPets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiFindPetsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFindPetsApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 
 }
